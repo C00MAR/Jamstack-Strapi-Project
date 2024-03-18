00cbb686fff3847c2d210cfb2ef903c6eee0495c
C00MAR <moureau.marc78@gmail.com>
2024-03-18 14:01:51 +0100

    :rocket: API Working (Missing Some Entry) :rocket:

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,18 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'img-src': ["'self'", 'data:', 'blob:', env('SUPABASE_API_URL')],
+          'media-src': ["'self'", 'data:', 'blob:', env('SUPABASE_API_URL')],
+        }
+      }
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +20,4 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
